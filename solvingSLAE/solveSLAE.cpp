be313025cba231409f68f7c3eabc317d517a70a7
Eduard Lazarev <seriousbanan@gmail.com>
2019-09-07 13:08:43 +0300

    Write definition of solveSLAE function

--- solvingSLAE/solveSLAE.cpp
+++ solvingSLAE/solveSLAE.cpp
@@ -1,9 +1,31 @@
 #include "solveSLAE.h"
 
-std::string getOneSolution(Matrix<int> &A, Matrix<int> &S);
-std::string getInfSolution(Matrix<int> &A, Matrix<int> &S);
+std::string getOneSolution(Matrix<int> &AS);
+std::string getInfSolution(Matrix<int> &AS, int countFreeVar);
 
-std::string solveSLAE(Matrix<int> &A, Matrix<int> &S)
+std::string solveSLAE(Matrix<int> &A, std::vector<int> &S)
 {
-	return "null";
-} 
+	int rangA{ A.rang() };
+	int Xlen = A.cols;
+	int rangAS;
+
+	//Create Advanced Matrix
+	Matrix<int> AS{ A.rows(), A.cols() + 1 };
+
+	for (int i{ 0 }; i < A.rows(); ++i)
+		for (int j{ 0 }; j < A.cols(); ++j)
+				AS[i][j] = A[i][j];
+	AS.insertCol(AS.cols - 1, S);
+
+	rangAS = AS.rang();
+
+	//Check existence of solution
+	if (rangA != rangAS)
+		return "The system haven't got solution";
+
+	//Check count of solution
+	if (rangA == Xlen)
+		return getOneSolution(AS);
+	else
+		return getInfSolution(AS, Xlen - rangA);
+}
--- solvingSLAE/solveSLAE.h
+++ solvingSLAE/solveSLAE.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../matrix-library-cpp/Matrix.h"
+#include "matrix-library-cpp/Matrix.h"
 #include <string>
 
 //This function get 2 matrix: coefficient matrix; free member matrix. And return string with solution of SLAE
-std::string solveSLAE(Matrix<int> &A, Matrix<int> &S);
+std::string solveSLAE(Matrix<int> &A, std::vector<int> &S);
