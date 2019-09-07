47355e2c2c8a4ce332e5995ce24caf679054b724
Eduard Lazarev <seriousbanan@gmail.com>
2019-09-07 21:00:57 +0300

    Add arithmetical functions (GCF, LCM, vector + vector, vector * int)

--- solvingSLAE/solveSLAE.cpp
+++ solvingSLAE/solveSLAE.cpp
@@ -1,21 +1,18 @@
 #include "solveSLAE.h"
 
-std::string getOneSolution(Matrix<int> &AS);
-std::string getInfSolution(Matrix<int> &AS, int countFreeVar);
-
 std::string solveSLAE(Matrix<int> &A, std::vector<int> &S)
 {
-	int rangA{ A.rang() };
-	int Xlen = A.cols;
-	int rangAS;
+	size_t rangA{ A.rang() };
+	size_t Xlen = A.cols();
+	size_t rangAS;
 
 	//Create Advanced Matrix
 	Matrix<int> AS{ A.rows(), A.cols() + 1 };
 
-	for (int i{ 0 }; i < A.rows(); ++i)
-		for (int j{ 0 }; j < A.cols(); ++j)
+	for (size_t i{ 0 }; i < A.rows(); ++i)
+		for (size_t j{ 0 }; j < A.cols(); ++j)
 				AS[i][j] = A[i][j];
-	AS.insertCol(AS.cols - 1, S);
+	AS.insertCol(AS.cols() - 1, S);
 
 	rangAS = AS.rang();
 
@@ -29,3 +26,67 @@ std::string solveSLAE(Matrix<int> &A, std::vector<int> &S)
 	else
 		return getInfSolution(AS, Xlen - rangA);
 }
+
+std::vector<int> operator+(std::vector<int> &row1, std::vector<int> &row2)
+{
+	if (row1.size() != row2.size())
+		throw(std::out_of_range("Wrong dimention"));
+
+	std::vector<int> res(row1);
+
+	for (size_t i{ 0 }; i < res.size(); ++i)
+		res[i] += row2[i];
+
+	return res;
+}
+
+std::vector<int> operator*(std::vector<int> &row, int k)
+{
+	std::vector<int> res(row);
+
+	for (size_t i{ 0 }; i < res.size(); ++i)
+		res[i] *= k;
+
+	return res;
+}
+
+int GCF(int a, int b)
+{
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
+	if (a == b)
+		return a;
+
+	if (a == 1 || b == 1)
+		return 1;
+
+	if (a % 2 == 0 && b % 2 == 0)
+		return 2 * GCF(a / 2, b / 2);
+	if (a % 2 == 0)
+		return GCF(a / 2, b);
+	if (a % 2 == 0)
+		return GCF(a, b / 2);
+
+	if (a > b)
+		return GCF((a - b) / 2, b);
+	if (a < b)
+		return GCF(a, (b - a) / 2);
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCF(a, b);
+}
+
+std::string getOneSolution(Matrix<int> &AS)
+{
+	return "null";
+}
+
+std::string getInfSolution(Matrix<int> &AS, int countFreeVar)
+{
+	return "null";
+}
+
--- solvingSLAE/solveSLAE.h
+++ solvingSLAE/solveSLAE.h
@@ -3,5 +3,18 @@
 #include "matrix-library-cpp/Matrix.h"
 #include <string>
 
-//This function get 2 matrix: coefficient matrix; free member matrix. And return string with solution of SLAE
+//This function get 2 matrix: coefficient matrix; free member matrix. And return string with solution of SLAE.
 std::string solveSLAE(Matrix<int> &A, std::vector<int> &S);
+
+//This function add two rows
+std::vector<int> operator+(std::vector<int> &row1, std::vector<int> &row2);
+
+//This function multiply every element in row to number
+std::vector<int> operator*(std::vector<int> &row, int k);
+
+int GCF(int a, int b);
+int LCM(int a, int b);
+
+//These functions return a solution depending on the number of solutions.
+std::string getOneSolution(Matrix<int> &AS);
+std::string getInfSolution(Matrix<int> &AS, int countFreeVar);
